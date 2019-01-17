bf4d451c11bfb8c53c62aa29bcaa409a900eea3f
Konstantin Baierer <unixprog@gmail.com>
2019-01-17 07:57:42 +0100

    add --watch option to start-server cli

--- mollusc-cli/src/cmds/start-server.js
+++ mollusc-cli/src/cmds/start-server.js
@@ -5,6 +5,12 @@ module.exports = {
   builder(yargs) {
     // console.log({yargs})
     yargs.options({
+      watch: {
+        alias: ['-w'],
+        type: 'boolean',
+        desc: 'Restart on changes to source code in $PWD',
+        default: false
+      },
       port: {
         desc: 'Port to start server on',
         default: 3434
@@ -13,6 +19,14 @@ module.exports = {
   },
   handler(args) {
     // console.log({args})
+    if (args.watch) {
+      const {execSync} = require('child_process')
+      const {quote} = require('shell-quote')
+      const cmd = `nodemon -x "${quote(process.argv)} --no-watch"`
+      console.log(cmd)
+      execSync(cmd, {stdio: 'inherit'})
+      process.exit()
+    }
     const server = new MolluscServer()
     server.start(args.port)
   }
