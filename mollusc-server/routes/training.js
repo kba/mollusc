540f646eed8930bddcfe563dcf5bfbdd3382a0ff
Konstantin Baierer <unixprog@gmail.com>
2019-02-04 11:25:29 +0100

    conneg checkpoints from training

--- mollusc-server/routes/training.js
+++ mollusc-server/routes/training.js
@@ -1,3 +1,4 @@
+const {createReadStream, stat} = require('fs')
 const {Router} = require('express')
 
 module.exports = function trainingRoute(server) {
@@ -6,14 +7,14 @@ module.exports = function trainingRoute(server) {
 
   function sessionMiddleware(req, resp, next) {
     const id = req.params.id
-    const instance = (id === 'latest')
-      ? engineManager.listInstances().pop()
-      : engineManager.getInstanceById(id)
-    // console.log({id, instance})
-    if (!instance)
+    const trainingInstance = (id === 'latest')
+      ? engineManager.listTrainingInstances().pop()
+      : engineManager.getTrainingInstanceById(id)
+    // console.log({id, trainingInstance})
+    if (!trainingInstance)
       return resp.status(404).send("No such session")
-    const {session} = instance
-    Object.assign(req, {session, instance})
+    const {session} = trainingInstance
+    Object.assign(req, {session, instance: trainingInstance})
     next()
   }
 
@@ -26,6 +27,30 @@ module.exports = function trainingRoute(server) {
 
   app.get('/:id', sessionMiddleware, (req, resp) => {
     return sendSession(req, resp)
+  })
+
+  app.get('/:id/checkpoint', sessionMiddleware, (req, resp) => {
+    const {checkpoints} = req.session
+    resp.set('Content-Type', 'application/json')
+    const urls = checkpoints.map(([dt, {path}], idx) => {
+      return `${baseUrl}/training/${req.session.id}/checkpoint/${idx}`
+    })
+    resp.send(urls)
+  })
+
+  app.get('/:id/checkpoint/:idx', sessionMiddleware, (req, resp) => {
+    const checkpoint = req.session.checkpoints[req.params.idx]
+    if (!checkpoint) {
+      return resp.status(404).send("Checkpoint not found")
+    }
+    const [dt, {mediaType, path}] = checkpoint
+    resp.set('Content-Type', mediaType)
+    resp.set('Date', dt)
+    stat(path, (err, {size}) => {
+      resp.set('Content-Length', size)
+      const instream = createReadStream(path)
+      instream.pipe(resp)
+    })
   })
 
   app.post('/:id/save', sessionMiddleware, (req, resp, next) => {
@@ -43,10 +68,9 @@ module.exports = function trainingRoute(server) {
 
   app.post('/', trafMiddleware, (req, resp) => {
     try {
-      const {id, session} = engineManager.createTrainingSession(req.body)
-      resp.set('Content-Type', 'application/json')
-      resp.set('Location', `${baseUrl}/training/${id}`)
-      resp.send(session)
+      const {session} = engineManager.createTrainingSession(req.body)
+      req.session = session
+      return sendSession(req, resp)
     } catch (error) {
       console.log(error)
       log.warn(error)
@@ -56,7 +80,7 @@ module.exports = function trainingRoute(server) {
   })
 
   app.get('/', (req, resp) => {
-    const sessions = engineManager.listInstances().map(instance => instance.session)
+    const sessions = engineManager.listTrainingInstances().map(instance => instance.session)
     if (req.query.full == '1') {
       resp.send(sessions)
     } else  {
